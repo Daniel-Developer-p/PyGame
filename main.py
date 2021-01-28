ba00f15a997f1fe204a855e3699c76af7d71e801
Tchannikov Daniel <75910357+Daniel-Developer-p@users.noreply.github.com>
2021-01-28 18:52:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ clock = pygame.time.Clock()
 
 panel_w = 330
 panel_h = 35
-panel_speed = 15
+panel_speed = 5
 panel = pygame.Rect(width // 2 - panel_w//2, height - panel_h - 10, panel_w, panel_h)
 
 ball_radius = 15
@@ -19,6 +19,34 @@ v_ball = 1
 dx, dy = 1, -1
 ball_rect = ball_radius * 2
 ball = pygame.Rect(rnd(ball_rect, width - ball_rect), height // 2, ball_rect, ball_rect)
+
+blocks = []
+for j in range(5):
+    Y = 10 + 70 * j
+    for i in range(10):
+        X = 10 + 120 * i
+        bl = pygame.Rect(X, Y, 100, 50)
+        blocks.append(bl)
+
+
+def collision(dx, dy, ball, block):
+    if dx > 0:
+        delta_x = ball.right - block.left
+    else:
+        delta_x = block.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - block.top
+    else:
+        delta_y = block.bottom - ball.top
+
+    if abs(delta_x, delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
 
 while True:
     for event in pygame.event.get():
@@ -33,19 +61,11 @@ while True:
 
     if ball.centerx < ball_radius or ball.centerx > width - ball_radius:
         dx = -dx
-    if ball.centery < ball_radius or ball.centery > height - ball_radius:
+    if ball.centery < ball_radius:
         dy = -dy
 
     if ball.colliderect(panel) and dy > 0:
         dy = -dy
-
-    blocks = []
-    for j in range(5):
-        Y = 10+70*j
-        for i in range(10):
-            X = 10 + 120*i
-            bl = pygame.Rect(X, Y, 100, 50)
-            blocks.append(bl)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and panel.left > 0:
@@ -54,3 +74,4 @@ while True:
         panel.right += panel_speed
     pygame.display.flip()
     clock.tick(60)
+
